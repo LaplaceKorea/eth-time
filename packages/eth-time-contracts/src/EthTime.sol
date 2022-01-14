1b488373e2eafeaf78799a03cb98a80595db4736
Francesco Ceccon <francesco@ceccon.me>
2022-01-14 21:06:18 +0000

    feat: display dots

--- packages/eth-time-contracts/src/EthTime.sol
+++ packages/eth-time-contracts/src/EthTime.sol
@@ -113,6 +113,11 @@ contract EthTime is ERC721("ETH Time", "ETHT") {
         historyAccumulator[id] ^= uint160(to);
     }
 
+    bytes constant groupOpenStart = '<g transform="translate(';
+    bytes constant groupOpenEnd = ')">';
+    bytes constant groupClose = '</g>';
+    bytes constant dots = '<circle cx="20" cy="36" r="6" fill="#FFFFFF"/><circle cx="20" cy="64" r="6" fill="#FFFFFF"/>';
+
     /// @dev Generate the SVG image for the given NFT.
     /// @param id the NFT unique id.
     function _tokenImage(uint256 id)
@@ -132,8 +137,49 @@ contract EthTime is ERC721("ETH Time", "ETHT") {
                     '<stop offset="0%" stop-color="hsl(10, 100%, 50%)" /><stop offset="100%" stop-color="hsl(100, 100%, 50%)" />',
                     '</linearGradient>',
                     '<rect x="0" y="0" width="1000" height="1000" fill="url(#bg)" />',
+                    _dots(500, 500),
                     '</svg>'
                 )
             );
+    }
+
+    function _dots(uint16 x, uint16 y)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return
+            bytes.concat(
+                groupOpenStart,
+                _uintToBytes(x),
+                ',',
+                _uintToBytes(y),
+                groupOpenEnd,
+                dots,
+                groupClose
+            );
+    }
+
+    /// @dev Taken from DVD Logo NFT 0x68a65e7968fbacd2387dad316918da69a7011608
+    function _uintToBytes(uint n)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        if (n == 0) {
+            return bytes("0");
+        }
+        bytes memory reversed = new bytes(100);
+        uint len = 0;
+        while (n != 0) {
+            uint r = n % 10;
+            n = n / 10;
+            reversed[len++] = bytes1(uint8(48 + r));
+        }
+        bytes memory buf = new bytes(len);
+        for (uint i= 0; i < len; i++) {
+            buf[i] = reversed[len - i - 1];
+        }
+        return buf;
     }
 }
